943c0baca0da6c1018e2797e151185f77de17dd4
Manuel G. Alvarez <manuelg_11@outlook.com>
2018-11-26 16:41:06 -0500

    añade gitignore al proyecto

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Calendario//
+
 var mes_text = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 
 var dia_text = ["Dom", "Lun", "Mar", "Mie", "Juv", "Vie", "Sab"];
